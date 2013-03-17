6a911093b8db206bfc9981c36bfaa2fc1dec5a37
Gregor von Laszewski <laszewski@gmail.com>
2013-03-17 09:23:14 -0400

    splitting the utils into a new dir

--- cmd3/plugins/shell_opt_example.py
+++ cmd3/plugins/shell_opt_example.py
@@ -3,9 +3,12 @@ from docopt import docopt
 import inspect
 import sys
 import importlib
+from  util.decorators import help_method
+from  util.decorators import _get_doc_args
 
 class shell_opt_example:
 
+    @help_method
     def help_opt_example(self):
         """
         Usage:
@@ -21,27 +24,8 @@ class shell_opt_example:
           -r       make report
 
         """
-        print textwrap.dedent(self.help_opt_example.__doc__)
-
-    @staticmethod
-    def _get_doc_args(help,args):
-        arguments = docopt(textwrap.dedent(help.__doc__), argv=args)
-        return arguments
-
-    # def _get_class(self,meth):
-    #    for cls in inspect.getmro(meth.im_class):
-    #        if meth.__name__ in cls.__dict__: return cls
-    #    return None
 
     def do_opt_example(self, args):
 
-        # FOR NOW DELETE THE COMMENTS THEY ARE FOR AN EVENTUAL DECORATOR
-        #name = sys._getframe().f_code.co_name
-        #help_name = name.replace("do_", "help_")
-        # NOT YET SURE HOW TO GET MY OWN POINTER TO MY METHOD IA M IN
-        #cls=  self._get_class(self.do_opt_example)
-        #method = getattr(cls, help_name)
-        #arguments = self._get_doc_args(method,args)
-        
-        arguments = self._get_doc_args(self.help_opt_example,args)
+        arguments = _get_doc_args(self.help_opt_example,args)
         print(arguments)
